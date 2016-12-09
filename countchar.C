2f24a44930afc53fe60cf2ee1ca87f90f01c60c6
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-12-09 06:51:32 +0530

    Create countchar.C

---
+++ countchar.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+          char name[200];
+          int i,count=0;
+          scanf("%[^\n]s",name);
+          printf("Name=%s",name);
+          for(i=0;name[i]!='\0';i++)
+          {
+            if(name[i]!=' ')
+          count++;
+          }
+          printf("\ncount=%d",count);
+	return 0;
+}
